7ca1344dd8161151be61071db9ce6d03fb453269
lebedevOK <teh161@yandex.ru>
2023-12-05 23:47:49 +0300

    Add Locators.py

---
+++ utils/Locators.py
@@ -0,0 +1,8 @@
+class MainPageLocators:
+    REGISTRATION_BUTTON = "body.vsc-initialized:nth-child(2) section.container header.header___2hp_9:nth-child(1) div.headerContent___1Z5eK div.headerWelcome___1pEVc > a.headerWelcomeButton___1_AQE.headerWelcomeButtonReg___ysSZB:nth-child(2)"
+
+    LOGIN_BUTTON = "//a[contains(text(),'Вход')]"
+
+    PREMATCH_BUTTON = "/html[1]/body[1]/div[1]/main[1]/section[1]/div[2]/section[1]/aside[1]/nav[1]/a[1]"
+
+    LIVE_BUTTON = "/html[1]/body[1]/div[1]/main[1]/section[1]/div[2]/section[1]/aside[1]/nav[1]/a[2]"
